223162a4ee50f59ef2d9e859d7903947bc08e968
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-09-11 14:57:27 +0300

    progress update

--- src/main/java/com/springboot/blog/SpringbootBlogApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogApplication.java
@@ -2,8 +2,12 @@ package com.springboot.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.springboot.blog.entity"})  // scan JPA entities
 public class SpringbootBlogApplication {
 
 	public static void main(String[] args) {
