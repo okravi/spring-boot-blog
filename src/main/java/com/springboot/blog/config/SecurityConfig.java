08702a23646ec35b4c659d839fb89cb7e2e985f6
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-09-28 16:45:44 +0300

    switched to securityFilterChain

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -6,42 +6,30 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig{
+    
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers(HttpMethod.GET, "/api/**").permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
-    }
-    
-    @Override
+
     @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails  james = User.builder().username("james").password(passwordEncoder()
-                .encode("test-password")).roles("USER").build();
-        UserDetails  admin = User.builder().username("admin").password(passwordEncoder()
-                .encode("test-password")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(james, admin);
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+        return http.build();
     }
 }
