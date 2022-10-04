c302f47bf6ac64a61ac25d48521c46cad06516f9
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-10-04 15:03:15 +0300

    added code to insert metadata in tables

--- src/main/java/com/springboot/blog/SpringbootBlogApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogApplication.java
@@ -1,16 +1,21 @@
 package com.springboot.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
+
 @SpringBootApplication
 @EnableConfigurationProperties
 @EntityScan(basePackages = {"com.springboot.blog.entity"})  // scan JPA entities
-public class SpringbootBlogApplication {
+public class SpringbootBlogApplication implements CommandLineRunner{
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
@@ -19,5 +24,19 @@ public class SpringbootBlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogApplication.class, args);
 	}
+	
+	@Autowired
+	private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role adminRole = new Role();
+        adminRole.setName("ROLE_ADMIN");
+        roleRepository.save(adminRole);
+        
+        Role userRole = new Role();
+        userRole.setName("ROLE_USER");
+        roleRepository.save(userRole);      
+    }
 
 }
